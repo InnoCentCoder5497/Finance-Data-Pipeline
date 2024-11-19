dfb43a162d4b07111d4bd7339a4cbb3a73a840ce
InnoCentCoder5497 <innocentcoder5497@gmail.com>
2024-11-19 11:18:06 +0530

    Added postgres containers and connection from spark

--- run_spark_submit.py
+++ run_spark_submit.py
@@ -2,11 +2,12 @@ import subprocess
 import argparse
 
 TEST_SCRIPT_LOC = '/scripts/test.py'
+JDBC_TEST_SCRIPT_LOC = '/scripts/test-cluster.py'
 PREPARE_TRANSACTIONS_SCRIPT = '/scripts/prepare_transactions.py'
 
 def run_job(name: str) -> None :
-    subprocess.run(f'docker exec -it spark_cluster-spark-1 spark-submit --master spark://spark:7077 {name} ')
+    subprocess.run(f'docker exec -it spark_cluster-spark-1 spark-submit --master spark://spark:7077 --jars /extra_jars/postgresql-42.7.4.jar {name}')
 
 
 if __name__ == '__main__':
-    run_job(PREPARE_TRANSACTIONS_SCRIPT)
+    run_job(JDBC_TEST_SCRIPT_LOC)
--- scripts/test-cluster.py
+++ scripts/test-cluster.py
@@ -3,24 +3,17 @@ from operator import add
 
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.appName('PhonPi') \
+spark = SparkSession.builder.appName('test-') \
     .master('spark://spark:7077') \
 .getOrCreate()
 
-df = spark.read.csv('/data/raw/transactions_data.csv')
-
-partitions = 5
-n = 100000 * partitions
-
-def f(_: int) -> float:
-    x = random() * 2 - 1
-    y = random() * 2 - 1
-    return 1 if x ** 2 + y ** 2 <= 1 else 0
-
-count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
-# time.sleep(30)
-print("Pi is roughly %f" % (4.0 * count / n))
-
-print(f'Data count: {df.count()}')
+jdbcDF = spark.read \
+    .format("jdbc") \
+    .option("url", "jdbc:postgresql://spark_cluster-db-warehouse-1:5432/postgres") \
+    .option("dbtable", "sample") \
+    .option("user", "postgres") \
+    .option("password", "postgres_pass") \
+    .load()
+jdbcDF.show()
 
 spark.stop()
