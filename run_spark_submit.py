71b0e0fdcf44ad42e5b5c7cb14ed189390a8ec89
InnoCentCoder5497 <innocentcoder5497@gmail.com>
2024-11-22 13:43:53 +0530

    Fix write to postgres

--- run_spark_submit.py
+++ run_spark_submit.py
@@ -7,7 +7,7 @@ PREPARE_TRANSACTIONS_SCRIPT = '/scripts/prepare_transactions.py'
 MCC_LOAD_SCRIPT = '/scripts/load_mcc.py'
 
 def run_job(name: str) -> None :
-    subprocess.run(f'docker exec -it spark_cluster-spark-1 spark-submit --driver-class-path /extra_jars/postgresql-9.4.1207.jar --master spark://spark:7077 {name}')
+    subprocess.run(f'docker exec -it spark_cluster-spark-1 spark-submit --driver-class-path /extra_jars/postgresql-42.7.4.jar --jars /extra_jars/postgresql-42.7.4.jar --master spark://spark:7077 {name}')
 
 
 if __name__ == '__main__':
--- scripts/load_mcc.py
+++ scripts/load_mcc.py
@@ -1,27 +1,42 @@
 from pyspark.sql import SparkSession
 import json
 
-spark = SparkSession.builder.appName('EDA').master('local[*]').getOrCreate()
+def write_rows_to_db(df, table_name, mode='append'):
+    df.distinct().write.format("jdbc") \
+        .option("url", "jdbc:postgresql://spark_cluster-db-warehouse-1:5432/postgres") \
+        .option("dbtable", table_name) \
+        .option("user", "postgres") \
+        .option("password", "postgres_pass") \
+        .mode(mode) \
+        .save()
 
-MCC_CODES_LOC = '/data/raw/mcc_codes.json'
+def main():
+    spark = SparkSession.builder.appName('Load MCC').master('spark://spark:7077').getOrCreate()
+    spark.sparkContext.setLogLevel("WARN")
 
-with open(MCC_CODES_LOC, 'r') as f:
-    data = json.load(f)
-    corrected_data = []
-    for key, value in data.items():
-        corrected_data.append((int(key), value))
+    print('Correcting MCC file data')
+    MCC_CODES_LOC = '/data/raw/mcc_codes.json'
 
-mcc_schema = 'mcc_code: int, mcc_desc:string'
-mcc_df = spark.createDataFrame(corrected_data, mcc_schema)
+    with open(MCC_CODES_LOC, 'r') as f:
+        data = json.load(f)
+        corrected_data = []
+        for key, value in data.items():
+            corrected_data.append((int(key), value))
 
-print(f'Loading {mcc_df.count()} Records in warehouse')
+    mcc_schema = 'mcc_code: int, mcc_desc:string'
+    mcc_df = spark.createDataFrame(corrected_data, mcc_schema)
 
-mcc_df.distinct().write.format("jdbc") \
-.option("url", "jdbc:postgresql://spark_cluster-db-warehouse-1:5432/postgres") \
-.option("dbtable", "mcc") \
-.option("user", "postgres") \
-.option("password", "postgres_pass") \
-.mode("append") \
-.save()
+    print(f'Loading {mcc_df.count()} Records in warehouse')
 
-spark.stop()
+    write_rows_to_db(mcc_df, 'mcc')
+    
+    print('MCC load complete')
+
+    spark.stop()
+    
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        print(e)
+        raise
