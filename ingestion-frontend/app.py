025539c40dadc4be5c647e8d12f6c827b9041348
InnoCentCoder5497 <innocentcoder5497@gmail.com>
2024-11-22 21:36:28 +0530

    Added frontend templates and static file support

--- ingestion-frontend/app.py
+++ ingestion-frontend/app.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
     return '<h1>Hello from Flask & Docker</h2>'
+
+@app.route('/hello')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', person=name)
 
 
 if __name__ == "__main__":
